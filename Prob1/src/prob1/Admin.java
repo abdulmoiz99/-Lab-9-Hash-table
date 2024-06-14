b32e5d67cfd3fa12484316fad96070d40579b8de
Abdul Moiz <moiza0799@gmail.com>
2024-06-14 12:04:28 -0500

    Problem 1 Solution

--- Prob1/src/prob1/Admin.java
+++ Prob1/src/prob1/Admin.java
@@ -2,7 +2,12 @@ package prob1;
 import java.util.*;
 public class Admin {
 	public static HashMap<Key, Student> processStudents(List<Student> students) {
-		//implement
-		return null;
+		HashMap<Key, Student> result = new HashMap<>();
+
+		for (Student student : students) {
+			result.put(new Key(student.getFirstName(), student.getLastName()), student);
+		}
+
+		return result;
 	}
 }
